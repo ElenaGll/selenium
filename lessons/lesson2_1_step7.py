5f74c14a216eb10cf7f4626d91191b9194580c2f
Elena Donyakina <hellengoll@gmail.com>
2023-04-15 23:51:11 +0300

    Added new files

--- lessons/lesson2_1_step7.py
+++ lessons/lesson2_1_step7.py
@@ -3,32 +3,32 @@ import time
 import math
 
 
-link = 'https://suninjuly.github.io/math.html'
+link = 'http://suninjuly.github.io/get_attribute.html'
 
 with webdriver.Chrome() as browser:
     browser.get(link)
 
-    x_element = browser.find_element(by='id', value='input_value')
-    x = x_element.text
+    treasure_box = browser.find_element(by='id', value='treasure')
+    value_x = treasure_box.get_attribute('valuex')
 
     def calculate(value):
-        return str(math.log(abs(12*math.sin(int(x)))))
+        return str(math.log(abs(12*math.sin(int(value)))))
 
-    answer = calculate(x)
+    answer = calculate(value_x)
     time.sleep(0.5)
 
     input1 = browser.find_element(by='id', value='answer')
     input1.send_keys(answer)
     time.sleep(0.5)
 
-    check_robot = browser.find_element(by='css selector', value='[for="robotCheckbox"]')
+    check_robot = browser.find_element(by='id', value='robotCheckbox')
     check_robot.click()
     time.sleep(0.5)
 
-    radio_robot = browser.find_element(by='css selector', value='[for="robotsRule"]')
+    radio_robot = browser.find_element(by='id', value='robotsRule')
     radio_robot.click()
     time.sleep(0.5)
 
     submit = browser.find_element(by='css selector', value='button.btn')
     submit.click()
-    time.sleep(1)
+    time.sleep(2)
