0cde22f6bf3e386aa7a42937cfeae007d7f0d2a6
Elena Donyakina <hellengoll@gmail.com>
2023-04-15 22:14:43 +0300

    added new files

---
+++ lessons/lesson1_6_step10.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+link = 'http://suninjuly.github.io/registration1.html'
+
+with webdriver.Chrome() as browser:
+    browser.get(link)
+    time.sleep(2)
+
+    input1 = browser.find_element(by='css selector', value='[placeholder="Input your first name"]')
+    input1.send_keys('Elena')
+
+    input2 = browser.find_element(by='css selector', value='[placeholder="Input your last name"]')
+    input2.send_keys('Donyakina')
+
+    input3 = browser.find_element(by='css selector', value='[placeholder="Input your email"]')
+    input3.send_keys('home@gmail.com')
+
+    button = browser.find_element(by='css selector', value='button.btn')
+    button.click()
+    time.sleep(3)
+
+    welcome_text_elt = browser.find_element(by='tag name', value='h1')
+    welcome_text = welcome_text_elt.text
+
+    assert 'Congratulations! You have successfully registered!' == welcome_text
+    time.sleep(2)
---
+++ lessons/lesson1_6_step11.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+link1 = 'http://suninjuly.github.io/registration1.html'
+link2 = 'http://suninjuly.github.io/registration2.html'
+
+with webdriver.Chrome() as browser:
+    browser.get(link1)
+    time.sleep(2)
+
+    input1 = browser.find_element(by='css selector', value='div.first_block input.form-control.first')
+    input1.send_keys('Elena')
+
+    input2 = browser.find_element(by='css selector', value='div.first_block input.form-control.second')
+    input2.send_keys('Donyakina')
+
+    input3 = browser.find_element(by='css selector', value='div.first_block input.form-control.third')
+    input3.send_keys('home@gmail.com')
+
+    button = browser.find_element(by='css selector', value='button.btn')
+    button.click()
+    time.sleep(3)
+
+    welcome_text_elt = browser.find_element(by='tag name', value='h1')
+    welcome_text = welcome_text_elt.text
+
+    assert 'Congratulations! You have successfully registered!' == welcome_text
+    time.sleep(2)
