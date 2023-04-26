6c2c7aa80ff4701ee211c09ef01bd3047bbc0ac3
Elena Donyakina <hellengoll@gmail.com>
2023-04-26 10:51:07 +0300

    fixed congtst file

--- conftest.py
+++ conftest.py
@@ -1,9 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 
 @pytest.fixture(scope='function')
 def browser():
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
     yield browser
     browser.quit()
