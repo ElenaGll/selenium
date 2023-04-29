f0f0e624da9b62d920c9d40f7bb89deaa3690e66
Elena Donyakina <hellengoll@gmail.com>
2023-04-29 04:15:03 +0300

    added files to review

--- conftest.py
+++ conftest.py
@@ -1,31 +1,32 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help='Choose browser: chrome or firefox')
+    parser.addoption('--language', default='ru',
+                     help='Choose language: ru, en, es, fr')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
     browser = None
 
     if browser_name == 'chrome':
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=chrome_options)
 
     elif browser_name == 'firefox':
-        options = Options()
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        browser = webdriver.Firefox(options=options)
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
 
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_guest_should_see_basket_button(browser):
+    browser.get(link)
+    time.sleep(10)
+
+    assert browser.find_element(by='css selector', value='button.btn-add-to-basket')
